f5bcba08006473295a9f51cd5a0bbaec9fb83fdd
Andrew Reed <w.andrew.reed@gmail.com>
2014-12-15 14:31:31 +0700

    implement LDW operation
    Add a memory setOcta helper to set 8 memory cells aligned on an octabyte.
    Add an address utility that converts an long to an octabyte hex string
    for standard memory address keys in the memory map.
    The test for LDW is from Knuth's MMIX pdf.

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ var uint64 = function(hex) {
   return Long.fromString(hex, true, 16);
 };
 
-var d = '0000000000000000';
+var padOcta = (function() {
+  var d = '0000000000000000';
 
-var padOcta = function(s) {
-  return d.split('').concat(s.split('')).slice(s.length).join('');
+  return function(s) {
+    return d.split('').concat(s.split('')).slice(s.length).join('');
+  };
+})();
+
+var address = function(l) {
+  return padOcta(l.toString(16));
 };
 
 /**
@@ -24,7 +30,23 @@ var padOcta = function(s) {
  * @alias module:mmix
  */
 function MMIX() {
-  this.memory = {};
+  var memory = this.memory = {};
+  this.memory.setByte = function(data, addr) {
+    if (data.length !== 2) {
+      throw new Error('Setting a byte memory requires a 1 byte valute.');
+    }
+    memory[addr] = data;
+  };
+  this.memory.setOcta = function(data, addr) {
+    data = padOcta(data);
+    var l = uint64(addr);
+    var offset = l.modulo(8);
+    var effective = l.subtract(offset);
+    for (var i = 0; i < 8; i++) {
+      var d = data.substring(i*2, (i*2) + 2);
+      memory.setByte(d, address(effective.add(i)));
+    }
+  };
   this.registers = {};
 }
 
@@ -47,8 +69,32 @@ MMIX.prototype.LDB = function(X, Y, Z) {
  
   var y = uint64(Y);
   var z = uint64(Z);
-  var address = padOcta(y.add(z).toString(16));
-  this.registers[X] = this.memory[address];
+  var A = address(y.add(z));
+  this.registers[X] = this.memory[A];
+};
+
+/**
+ * Load the wyde at memory address Y + Z into register X.
+ * @param {Register} X
+ * @param {Octabyte} Y
+ * @param {Octabyte} Z
+ */
+MMIX.prototype.LDW = function(X, Y, Z) {
+  if (typeof registers[X] === 'undefined') {
+    throw new Error('The machine does not have a register named ' + X);
+  }
+  if (typeof Y !== 'string' || Y.length !== 2) {
+    throw new Error(Y + ' should be a single byte hex string.');
+  }
+  if (typeof Z !== 'string' || Z.length !== 2) {
+    throw new Error(Z + ' should be a single byte hex string.');
+  }
+
+  var y = uint64(Y);
+  var z = uint64(Z);
+  var A1 = address(y.add(z));
+  var A2 = address(y.add(z.add(1)));
+  this.registers[X] = padOcta(this.memory[A1] + this.memory[A2]);
 };
 
 module.exports = MMIX;
--- test/index.js
+++ test/index.js
@@ -30,3 +30,32 @@ describe('LDB', function() {
     });
   });
 });
+
+describe('LDW', function() {
+  var mmix = new MMIX();
+  var tests = [
+    //register, Y operand, Z operand, M8 address, data, correct register value
+    ['$1', '00', '02', '0000000000000002', '0123456789ABCDEF', '0000000000004567']
+  ];
+  var X = nth(0);
+  var Y = nth(1);
+  var Z = nth(2);
+  var address = nth(3);
+  var data = nth(4);
+  var answer = nth(5);
+
+  tests.forEach(function(t) {
+    describe(['Memory8[', address(t), '] = ', data(t)].join(''), function() {
+      before(function() {
+        mmix.memory.setOcta(data(t), address(t));
+      });
+
+      describe(['LDW', X(t), Y(t), Z(t)].join(' '), function() {
+        it(['should set register', X(t), 'to', answer(t)].join(' '), function() {
+          mmix.LDW(X(t), Y(t), Z(t));
+          expect(mmix.registers[X(t)]).to.equal(answer(t));
+        });
+      });
+    });
+  });
+});

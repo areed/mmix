de01a68dd7141e7766f221b3b8f9923a6e50482f
Andrew Reed <w.andrew.reed@gmail.com>
2014-12-22 12:29:07 +0700

    quotient and remainder utilities for big ints

--- test/utils.js
+++ test/utils.js
@@ -28,9 +28,56 @@ describe('Utils', function() {
       ].forEach(function(r) {
         var width = r[0];
         var answer = r[1];
-        it(['(', width, input, ') =>', answer].join(' '), function() {
+        it(['(', width, input, ') ==', answer].join(' '), function() {
           expect(effective(width, input).toString()).to.equal(answer.toString());
         });
+      });
+    });
+  });
+
+  //quotient, remainder
+  var qrTests = [
+    //n, divisor, quotient, remainder
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [1, 2, 0, 1],
+    [2, 1, 2, 0],
+    [3, 2, 1, 1],
+    [-1, 1, -1, 0],
+    [-1, 2, -1, 1],
+    [-2, 1, -2, 0],
+    [-3, 2, -2, 1],
+    [0, -1, 0, 0],
+    [1, -1, -1, 0],
+    [1, -2, 0, 1],
+    [2, -1, -2, 0],
+    [3, -2, -1, 1],
+  ];
+
+  describe('quotient', function() {
+    qrTests.forEach(function(t) {
+      var n = Big(t[0]);
+      var divisor = Big(t[1]);
+      var quotient = Big(t[2]);
+      var remainder = Big(t[3]);
+
+      it(['(', divisor, n, ') ==', quotient].join(' '), function() {
+        expect(utils.quotient(divisor, n).toString()).to.equal(quotient.toString());
+      });
+    });
+  });
+
+  describe('remainder', function() {
+    qrTests.forEach(function(t) {
+      var n = Big(t[0]);
+      var divisor = Big(t[1]);
+      var quotient = Big(t[2]);
+      var remainder = Big(t[3]);
+
+      it(['(', divisor, n, ') ==', remainder].join(' '), function() {
+        expect(utils.remainder(divisor, n).toString()).to.equal(remainder.toString());
+        //n = qd + r
+        expect(quotient.times(divisor).plus(remainder).toString()).to.equal(n.toString());
       });
     });
   });
--- utils.js
+++ utils.js
@@ -8,3 +8,24 @@ var Big = require('big.js');
 exports.effectiveAddress = function(byteWidth, addr) {
   return addr.minus(addr.mod(byteWidth));
 };
+
+/**
+ * @param {Int} n
+ * @return {Int}
+ */
+var quotient = exports.quotient = function(divisor, n) {
+  var q = n.div(divisor);
+
+  return n.cmp(0) === -1 ? q.round(0, 3) : q.round(0, 0);
+};
+
+/**
+ * @param {Int} divisor
+ * @param {Int} n
+ * @return {Int}
+ */
+exports.remainder = function(divisor, n) {
+  var q = quotient(divisor, n);
+
+  return n.minus(q.times(divisor));
+};
